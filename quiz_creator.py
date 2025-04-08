615a0d9b19f1df483bfac26e7c622a2fc555435a
Jasper <japeemga@gmail.com>
2025-04-08 15:26:21 +0800

    Pseudocode

---
+++ quiz_creator.py
@@ -0,0 +1,4 @@
+#make a function that asks user for a question, choices for the question, and the answer
+#save the question, choices, and answer to a text file
+#ask the user if they want to add another question
+#repeat until the user says no
