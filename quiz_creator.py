cbfdcde37d38b2ea9f6f49cb9e13c816bb7c98da
Jasper <japeemga@gmail.com>
2025-04-08 15:37:59 +0800

    Make a function that continously asks user to enter a question, then make an empty dictionary of choices, and make a list of labels - a,b,c,d

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,11 @@
 #make a function that asks user for a question, choices for the question, and the answer
+def quiz_maker():
+    while True:
+        question = input("Enter the question: ")
+        choices = {}
+        labels = ['a', 'b', 'c', 'd']
 #save the question, choices, and answer to a text file
 #ask the user if they want to add another question
 #repeat until the user says no
+#call the function
+quiz_maker()
