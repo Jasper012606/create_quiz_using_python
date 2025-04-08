d7af9cf09c26c63c5ad8b89f1da8a5175fcb0d8f
Jasper <japeemga@gmail.com>
2025-04-08 16:00:36 +0800

    Ask if user wants to add another question, if the user enters no, it exits the program

--- quiz_creator.py
+++ quiz_creator.py
@@ -5,7 +5,14 @@ def quiz_maker():
         choices = {}
         labels = ['a', 'b', 'c', 'd']
 #save the question, choices, and answer to a text file
-#ask the user if they want to add another question
+#ask the user if they want to add another question           
+        while True:
+            another_question = input("Do you want to add another question? (y/n): ").strip().lower()
+            if another_question == 'y':
+                break
+            elif another_question == 'n':
+                print("Exiting the quiz maker.")
+                exit()
 #repeat until the user says no
 #call the function
 quiz_maker()
