c1309b06d66e96a08ef82d08345e0d354d5b27b5
Jasper <japeemga@gmail.com>
2025-05-06 23:49:21 +0800

    Make a function that loads the quiz file, if no file was found, it asks the user to make a quiz file first

--- quiz_taker.py
+++ quiz_taker.py
@@ -1,4 +1,14 @@
 #make a function that loads and runs the quiz file
+def load_quiz():
     #check if the file exists
-    #if true, proceed to load the file
+    try:
+        #if true, proceed to load the file
+        with open("quiz.txt", "r") as quiz_file:
+            #read the file line by line
+            lines = quiz_file.readlines()
+            
     #else, notice the user that the file does not exist
+    except FileNotFoundError:
+        print("Quiz file not found. Please Create a Quiz first.")
+        return
+load_quiz()
