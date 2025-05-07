4c7331846ba8ac137bd91ad00d1d5e94ec6ee74f
Jasper <japeemga@gmail.com>
2025-05-07 13:56:54 +0800

    Initialize the scores, total questions, and number of lines to 0. Look for a line that starts with 'Question', if line starts with 'Question', Print the line/question

--- quiz_taker.py
+++ quiz_taker.py
@@ -11,4 +11,20 @@ def load_quiz():
     except FileNotFoundError:
         print("Quiz file not found. Please Create a Quiz first.")
         return
+    #initialize the score, total questions and number of lines to 0
+    score = 0
+    total_questions = 0
+    number_of_lines = 0
+    
+    while number_of_lines < len(lines):
+        line = lines[number_of_lines].strip()
+        #check if the line starts with "Question:", proceed if true
+        if line.startswith("Question:"):
+            #add 1 to the total questions
+            total_questions += 1
+            #print the question
+            question = line.strip().replace("Question:", f"Question {total_questions}:").strip()
+            print(f"\n{question}")
+            break
+        
 load_quiz()
